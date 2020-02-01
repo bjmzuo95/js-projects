fea7b51b907061353cc8691f7fe0a574954fec17
bjmzuo95 <60087439+bjmzuo95@users.noreply.github.com>
2020-02-01 22:27:16 +0900

    Add files via upload

--- Tic_Tac_Toe/sketch.js
+++ Tic_Tac_Toe/sketch.js
@@ -1,20 +1,71 @@
 let board = [
-    ['O', 'X', 'X'],
-    ['O', 'O', 'X'],
-    ['O', 'X', 'O'],
+    ['', '', ''],
+    ['', '', ''],
+    ['', '', ''],
 ];
 
 let players = ['O', 'X'];
 let currentPlayer;
+let available = [];
 
 function setup() {
     createCanvas(400, 400);
-    currentPlayer = random(players);
+    frameRate(1);
+    currentPlayer = floor(random(players.length));
+    for (let i = 0; i < 3; i++) {
+        for (let j = 0; j < 3; j++) {
+            available.push([i, j]);
+        }
+    }
+}
+
+function nextTurn() {
+    let index = floor(random(available.length));
+    let spot = available.splice(index, 1)[0];
+    let i = spot[0];
+    let j = spot[1];
+    board[i][j] = players[currentPlayer];
+    currentPlayer = (currentPlayer + 1) % players.length;
+}
+
+// function mousePressed() {
+//     nextTurn();
+// }
+
+function equals3(a, b, c) {
+    return (a == b && b == c && a != '');
+}
+
+function checkWinner() {
+    let winner = null;
+
+    for (let i = 0; i < 3; i++) {
+        if (equals3(board[i][0], board[i][1], board[i][2])) {
+            winner = board[i][0];
+        }
+    }
+    for (let i = 0; i < 3; i++) {
+        if (equals3(board[0][i], board[1][i], board[2][i])) {
+            winner = board[0][i];
+        }
+    }
+    if (equals3(board[0][0], board[1][1], board[2][2])) {
+        winner = board[0][0];
+    }
+    if (equals3(board[2][0], board[1][1], board[0][2])) {
+        winner = board[2][0];
+    }
+
+    if (winner == null && available.length == 0) {
+        return 'tie';
+    } else {
+        return winner;
+    }
 }
 
 function draw() {
     background(255);
-    strokeWeight(3);
+    strokeWeight(2);
 
     let w = width / 3;
     let h = height / 3;
@@ -38,9 +89,13 @@ function draw() {
                 line(x - xr, y - xr, x + xr, y + xr);
                 line(x + xr, y - xr, x - xr, y + xr);
             }
-
-            // textSize(32);
-            // text(spot, x, y);
         }
     }
+    
+    let result = checkWinner();
+    if (result != null) {
+        noLoop();
+        createP(result).style('font-size', '32pt');
+    }
+    nextTurn();
 }
