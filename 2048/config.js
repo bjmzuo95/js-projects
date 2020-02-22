c74c54bb0228950913e2f6aab84788106802beb6
bjmzuo95 <60087439+bjmzuo95@users.noreply.github.com>
2020-02-22 22:14:15 +0900

    Add files via upload

---
+++ 2048/config.js
@@ -0,0 +1,46 @@
+let colorsSizes = {
+  "2": {
+      size: 64,
+      color: "#FFF999"
+  },
+  "4": {
+      size: 64,
+      color: "#CCF9AA"
+  },
+  "8": {
+      size: 64,
+      color: "#49BB6C"
+  },
+  "16": {
+      size: 64,
+      color: "#2494C1"
+  },
+  "32": {
+      size: 64,
+      color: "#9659A7"
+  },
+  "64": {
+      size: 64,
+      color: "#F1C500"
+  },
+  "128": {
+      size: 48,
+      color: "#FF5956"
+  },
+  "256": {
+      size: 48,
+      color: "#F1C5FF"
+  },
+  "512": {
+      size: 48,
+      color: "#99BB9C"
+  },
+  "1024": {
+      size: 32,
+      color: "#24FFC1"
+  },
+  "2048": {
+      size: 32,
+      color: "#A659A9"
+  }
+}
